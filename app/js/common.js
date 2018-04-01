ad96470834d867b0738561022475d0ccc3cebe07
RomanChygryn <r.chigrin@gmail.com>
2018-04-01 08:09:01 +0200

    some changes

--- app/js/common.js
+++ app/js/common.js
@@ -22,10 +22,3 @@ $(document).ready(function(){
     } // End if
   });
 });
-
-
-
-var quadimages = document.querySelectorAll("#quad figure");
-for(i=0; i<quadimages.length; i++) {
-  quadimages[i].addEventListener('click', function(){ this.classList.toggle("expanded"); quad.classList.toggle("full") });
-}
